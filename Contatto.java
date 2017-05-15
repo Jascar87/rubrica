a15dfa5d67b85e5e6f0d06ec3bb2206d9378075b
Riccardo <riccardopidello@gmail.com>
2017-05-15 14:53:24 +0200

    Revert "Revert "mod""
    This reverts commit c3350fc764c760a036dae2641c778917a631b8f2.

---
+++ Contatto.java
@@ -0,0 +1,95 @@
+package rubrica;
+
+import java.util.ArrayList;
+/**
+ * La classe contiene il campo name con cui viene salvato il contato, il campo eMail dove contiene una eMail del contatto
+ * e contiene il campo telNum dove sono salvati tutti i numeri di telefono.
+ * La classe si occupa di mantenere lo stato del contatto aggiungendo e rimuovendo numeri di telefono, sostituendo la eMail.
+ * 
+ * @author Jacar
+ */
+public class Contatto {
+	public final String name;
+	private String eMail;
+	private ArrayList<String> telNums;
+	
+	/**
+	 * Costruttore del oggetto Contatto.
+	 * 
+	 * @param name è il nome con cui viene salvato il contatto
+	 * @param eMail è l'indirizzo email
+	 * @param telNums è la ArrayList dei numeri di telefono
+	 */
+	public Contatto(String name, String eMail, String telNums){
+		this.name = name;
+		this.eMail = eMail;
+		this.telNums = new ArrayList<String>();
+		String[] Nums = telNums.split(", ");
+		for (String temp : Nums){
+			this.telNums.add(temp);
+		}
+	}
+	/**
+	 * Fornisce la email del contatto.
+	 * 
+	 * @return la email del contato sottoforma di stringa
+	 */
+	public String getEMail(){
+		return eMail;
+	}
+	
+	/**
+	 * Imposta la email del contatto con la stringa fornita.
+	 * 
+	 * @param eMail stringa da inseire nel campo email 
+	 * @return valore booleano per indicare se l'operazione è andata a buon fine
+	 */
+	public boolean setEMail(String eMail){
+		if (eMail != null && eMail.matches(".*@.*\\..*")) {
+			this.eMail = eMail;
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Aggiunge alla lista dei numeri di telefono del contatto il numero fornito.
+	 *  
+	 * @param telNum è il numero da inserire 
+	 * @return valore booleano per indicare se l'operazione è andata a buon fine
+	 */
+	public boolean addTelNum(String telNum){
+		if (telNum != null && telNum.matches("[0-9]+") && !telNums.contains(telNum))
+			return this.telNums.add(telNum);
+		return false;
+	}
+	
+	/**
+	 * Rimuove dalla lista dei numeri di telefono del contatto il numero fornito.
+	 *  
+	 * @param telNum è il numero da rimuovere 
+	 * @return valore booleano per indicare se l'operazione è andata a buon fine
+	 */
+	public boolean delTelNum(String telNum){
+       return this.telNums.remove(telNum);
+    }
+         
+	/**
+	 * Fornisce una stringa con il primo numero di telefono.
+	 *  
+	 * @return la stringa del primo numero di telefono
+	 */
+	public String getTelNums(){
+		String temp = telNums.toString();
+		return temp.substring(1, temp.length()-1);
+	}
+	
+	/**
+	 * Fornisce una stringa con nome, email e telefono.
+	 * 
+	 * @return i valori di name, eMail, e il primo numero di telefono sottoforma di stringa
+	 */
+	public String toString(){
+		return name + "; " + eMail + "; " + getTelNums();
+	}
+}
